0af41a56a2c404e60092534922fbaa07728e21f2
Max Spiridonov <spiridonov.max@gmail.com>
2020-06-21 11:39:38 +0300

    fixed httperros ui error processor

--- example/s3/instrumenting.go
+++ example/s3/instrumenting.go
@@ -19,26 +19,31 @@ type instrumentingMiddleware struct {
 	svc         Service
 }
 
+// CreateMultipartUpload ...
 func (s *instrumentingMiddleware) CreateMultipartUpload(ctx context.Context, bucket string, key string) (data v1.CreateMultipartUploadData, errorFlag bool, errorText string, additionalErrors *v1.AdditionalErrors, err error) {
 	defer s.recordMetrics("CreateMultipartUpload", time.Now(), err)
 	return s.svc.CreateMultipartUpload(ctx, bucket, key)
 }
 
+// UploadPartDocument ...
 func (s *instrumentingMiddleware) UploadPartDocument(ctx context.Context, bucket string, key string, uploadID string, partNumber int64, document []byte) (err error) {
 	defer s.recordMetrics("UploadPartDocument", time.Now(), err)
 	return s.svc.UploadPartDocument(ctx, bucket, key, uploadID, partNumber, document)
 }
 
+// CompleteUpload ...
 func (s *instrumentingMiddleware) CompleteUpload(ctx context.Context, bucket string, key string, uploadID string) (err error) {
 	defer s.recordMetrics("CompleteUpload", time.Now(), err)
 	return s.svc.CompleteUpload(ctx, bucket, key, uploadID)
 }
 
+// UploadDocument ...
 func (s *instrumentingMiddleware) UploadDocument(ctx context.Context, bucket string, key string, document []byte) (err error) {
 	defer s.recordMetrics("UploadDocument", time.Now(), err)
 	return s.svc.UploadDocument(ctx, bucket, key, document)
 }
 
+// DownloadDocument ...
 func (s *instrumentingMiddleware) DownloadDocument(ctx context.Context, bucket string, key string) (document []byte, err error) {
 	defer s.recordMetrics("DownloadDocument", time.Now(), err)
 	return s.svc.DownloadDocument(ctx, bucket, key)
